da4c87c74dc85aabcff899c1b49b6c18b60ece91
oswaldpale <oswaldpale@hotmail.com>
2015-12-15 17:38:30 -0500

    creacion del proyecto

---
+++ App/PluginDigitalPersona/PluginDigitalPersona/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general sobre un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos atributos para modificar la información
+// asociada con un ensamblado.
+[assembly: AssemblyTitle("PluginDigitalPersona")]
+[assembly: AssemblyDescription("Puente de Conexion")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Gas Caqueta")]
+[assembly: AssemblyProduct("PluginDigitalPersona")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible como false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM. Si necesita obtener acceso a un tipo de este ensamblado desde 
+// COM, establezca el atributo ComVisible como true en este tipo.
+[assembly: ComVisible(true)]
+
+// El siguiente GUID sirve como identificador de typelib si este proyecto se expone a COM
+[assembly: Guid("010d735a-3daf-4e48-92d3-9cdf16dfe68e")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria 
+//      Número de compilación
+//      Revisión
+//
+// Puede especificar todos los valores o establecer como predeterminados los números de versión de compilación y de revisión 
+// mediante el asterisco ('*'), como se muestra a continuación:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ App/PluginDigitalPersona/PluginDigitalPersona/pluginDigitalpersona.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace PluginDigitalPersona
+
+{
+    public class pluginDigitalpersona: DPFP.Capture.EventHandler
+    {
+        private DPFP.Capture.Capture Capturer;
+        public pluginDigitalpersona() {
+            iniciarPlugin();
+        }
+
+        public void iniciarPlugin()
+        {
+          
+        }
+
+        #region Init Method
+        /// <summary>
+        /// Metodos de Control de eventos.
+        /// </summary>
+        /// 
+        protected virtual void Init()
+        {
+            try
+            {
+                Capturer = new DPFP.Capture.Capture();				// Create a capture operation.
+
+                if (null != Capturer)
+                    Capturer.EventHandler = this;					// Subscribe for capturing events.
+            }
+            catch
+            {
+               
+            }
+        }
+        protected void Start()
+		{
+            if (null != Capturer)
+            {
+                try
+                {
+                    Capturer.StartCapture();
+                  
+                }
+                catch
+                {
+                  
+                }
+            }
+		}
+
+		protected void Stop()
+		{
+            if (null != Capturer)
+            {
+                try
+                {
+                    Capturer.StopCapture();
+                }
+                catch
+                {
+                   
+                }
+            }
+		}
+        #endregion
+
+        #region WebForm Event Handlers:
+        /// <summary>
+        /// Construccion de los eventos de escucha del lector al inicializar.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+		private void CaptureForm_Load(object sender, EventArgs e)
+		{
+			Init();
+			Start();												// Start capture operation.
+		}
+	
+	#endregion
+
+
+        #region EventHandler Members
+        /// <summary>
+        /// Eventos En tiempo Real del Lector Biometrico...
+        /// </summary>
+        /// <param name="Capture"></param>
+        /// <param name="ReaderSerialNumber"></param>
+        /// <param name="Sample"></param>
+        public void OnComplete(object Capture, string ReaderSerialNumber, DPFP.Sample Sample)
+        {
+           
+          
+        }
+
+        public void OnFingerGone(object Capture, string ReaderSerialNumber)
+        {
+           
+        }
+
+        public void OnFingerTouch(object Capture, string ReaderSerialNumber)
+        {
+           
+        }
+
+        public void OnReaderConnect(object Capture, string ReaderSerialNumber)
+        {
+           
+        }
+
+        public void OnReaderDisconnect(object Capture, string ReaderSerialNumber)
+        {
+         
+        }
+        #endregion
+
+        #region Miembros de EventHandler
+
+
+        public void OnSampleQuality(object Capture, string ReaderSerialNumber, DPFP.Capture.CaptureFeedback CaptureFeedback)
+        {
+           
+        }
+
+        #endregion
+    }
+}
